be7a3193141d65b87e4dbf85adf588741b8261c7
Yogesh <ypadalkar95@gmail.com>
2023-02-05 18:33:31 +0530

    sinletcombination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,6 +1,37 @@
-FLIP=$(($(($RANDOM%10))%2))
-if [ $FLIP -eq 1 ];then
-    echo "heads"
+#varibles
+
+counter=0
+index=0
+total_heads=0
+total_tails=0
+
+#Dictionary for Singlet Combination
+declare -A singlet
+singlet[head]=" "
+singlet[tails]=" "
+singlet[h_per]=0
+singlet[t_per]=0
+
+read -p "Enter how many times you want flip the coin  " FLIP
+while [ $counter -lt $FLIP ]
+do
+	coin_1=$(($RANDOM%2))
+	if [ $coin_1 -eq 1 ]
+then
+		singlet[head]="H"
+		((total_heads++))
 else
-    echo "tails"
-fi
+		singlet[tails]="T"
+		((total_tails++))
+	fi
+        ((counter++))
+done
+echo "Combination of Singlet"
+echo "--------------------------------------"
+echo "Singlet Combination" ${singlet[head]} ${singlet[tails]}
+singlet[h_per]=$(echo $total_heads $FLIP | awk '{print $1/$2}')
+echo "Percentage of head ="${singlet[h_per]}
+
+singlet[t_per]=$(echo $total_tails $FLIP | awk '{print $1/$2}')
+echo "Percentage of tails =" ${singlet[t_per]}
+
